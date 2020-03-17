44b88210e2e1fa5db940e34ed41133cc00071fab
Amir Ben Youssef <amirbenyoussef3@gmail.com>
2020-03-17 20:19:41 +0100

    Amir Ben Youusef

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,5 @@
 
-
+/*
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -63,5 +63,66 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+var HashTable = function() {
+  this._limit = 8;
+  this._storage = new Array(this._limit);
+  for (var i = 0; i< this._storage.length ; i++){
+    this._storage[i] = new Map()
+  }
+};
+
+HashTable.prototype.insert = function(k, v) {
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage[index].set(k,v)
+};
+
+HashTable.prototype.retrieve = function(k) {
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage[index].get(k)
+};
+
+HashTable.prototype.remove = function(k) {
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var removed = this.retrieve(k)
+  this._storage[index].delete(k)
+  return removed;
+};
+
+
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ */
+
+
+Map()
+  }
+};
+
+HashTable.prototype.insert = function(k, v) {
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage[index].set(k,v)
+};
+
+HashTable.prototype.retrieve = function(k) {
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage[index].get(k)
+};
+
+HashTable.prototype.remove = function(k) {
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var removed = this.retrieve(k)
+  this._storage[index].delete(k)
+  return removed;
+};
+
+
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ */
+
 
 
